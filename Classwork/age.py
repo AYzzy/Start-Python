2044f792ce4301897156047016715f764ced7d1a
AYzzy <omoniyiayomide987@gmail.com>
2024-07-09 16:47:12 +0100

    commit

---
+++ Classwork/age.py
@@ -0,0 +1,13 @@
+age =int(input("Enter your damn age : "))
+
+if age >19 and age <=45:
+	print('You are eigible to make your own decision')
+	print('hello')
+	print('tech blazers')
+elif age >=13 and age <=19:
+	print('Ypu are a teenager')
+	print('hello')
+elif age > 0 and age <13:
+	print('TYou are still a child ')
+else:
+	print("Ypu are old")
---
+++ Classwork/name2.py
@@ -0,0 +1,4 @@
+surname = input('Enter your surname:')
+name = input('Enter your name : ')
+age = input ("Enter your age: ")
+print (f'Your fullname is {surname} {name} and Your age is {age}')
