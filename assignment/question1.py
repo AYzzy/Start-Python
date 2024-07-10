9fc42367d2b172f641823db2de2b370f8b762e2a
AYzzy <omoniyiayomide987@gmail.com>
2024-07-10 07:03:58 +0100

    commit

---
+++ assignment/question1.py
@@ -0,0 +1,6 @@
+#the code wouldnt read an integer because it sees the code as strings 
+# to convert that code to read int, it goes like this 
+
+rating =int(input("Enter an integer rating between 1 and 10: "))
+
+#Therr we have it 
---
+++ assignment/question9.py
@@ -0,0 +1,4 @@
+char= input("Enter character: ")
+
+main = ord(char)
+print(main)
